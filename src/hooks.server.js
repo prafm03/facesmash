dacc69d36c22322879b5c7b8c5ae3f0b86c8ef15
prafulcc <praful@tuta.io>
2024-02-10 21:25:01 +0000

    stopped creating a new database object on every visit

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,6 +2,8 @@ import { SECRET_DB_ENDPOINT } from "$env/static/private";
 import PocketBase from "pocketbase";
 
 export const handle = async ({ event, resolve }) => {
-  event.locals.db = new PocketBase(SECRET_DB_ENDPOINT);
+  if (!event.locals.db) {
+    event.locals.db = new PocketBase(SECRET_DB_ENDPOINT);
+  }
   return resolve(event);
 };
