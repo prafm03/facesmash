d93264aebef6d9267c06c4082815be4e48c14afb
prafulcc <praful@tuta.io>
2024-02-11 03:40:45 +0000

    new

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,9 +1,9 @@
-import { DB_ENDPOINT, DB_SECRET } from "$env/static/private";
+import { DB_URL, DB_SECRET } from "$env/static/private";
 import PocketBase from "pocketbase";
 
 export const handle = async ({ event, resolve }) => {
   if (!event.locals.db) {
-    event.locals.db = new PocketBase(DB_ENDPOINT);
+    event.locals.db = new PocketBase(DB_URL);
     event.locals.db.beforeSend = (url, options) => {
       options.headers["db-secret"] = DB_SECRET;
       return { url, options };
